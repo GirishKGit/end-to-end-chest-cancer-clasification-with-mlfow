cd4af3a5a7526f10027151fb10bc3cd7e6798333
Girish <kumargirish.rama@gmail.com>
2024-01-25 16:02:03 +0530

    commit1

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+import setuptools
+with open("READ.md",    "r",encoding = "uft-8") as f:
+    long_description = f.read()
+
+    __Version__= "0.0.0"
+
+    REPO_NAME= "Chest-Cancer-classificationProject"
+    
+
+    setuptools.setup 
+
+    name=   SRC_REPO,
+    version= __Version__,
+
+
+
+
+
+
+    )
---
+++ src/cnnclassifier/main.py
@@ -0,0 +1,2 @@
+from cnnclassifier import logger
+logger .info("hello")
